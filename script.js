4b13ad0b6b70dfe61c5a5501037527215fe54c15
Nikita Nishad <nikitanishad001@gmail.com>
2023-11-28 15:05:04 +0530

    adding Js,css

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const toggleButton = document.getElementById("nav-toggle");
+const list =document.getElementById('list');
+
+toggleButton.addEventListener('click',()=>{
+    list.classList.toggle('active');
+})
