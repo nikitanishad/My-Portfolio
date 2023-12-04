f838bed8254ed6f036f9301b4cfbe4f21e740b1d
Nikita Nishad <nikitanishad001@gmail.com>
2023-12-04 22:11:47 +0530

    little change

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
+
 const toggleButton = document.getElementById("nav-toggle");
 const list =document.getElementById('list');
 
 toggleButton.addEventListener('click',()=>{
     list.classList.toggle('active');
-})
+
+
+   // using progres bar
